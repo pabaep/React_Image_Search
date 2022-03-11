82ed8b413210b1d688d8e6e09a0f90130e033ae5
pabaep <sewon225@gmail.com>
2022-03-11 14:22:22 +0900

    [update] modal for confirm image
    확인 버튼을 눌렀을때 모달을 띄우고,
    이미지를 다운받을 수 있는 새창으로 연결함
    이미지 url을 props로 내려서 modal에서 띄우도록 변경

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './CartItem.module.css'
 
 const CartItem = (props) => {
-  const url = "https://cdn.pixabay.com/photo/2021/03/16/21/46/tea-6101059_960_720.jpg"
+  const url = props.url;
   return (
     <div className={classes.cartItem}>
       <img className={classes.img} src={url} alt="결과 이미지"/>
